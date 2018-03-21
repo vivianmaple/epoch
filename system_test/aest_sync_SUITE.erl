b7cd8db2481e962f8d65e9daf4137fd694b7b6ea
Thomas Arts <thomas.arts@quviq.com>
2018-03-21 15:38:48 +0100

    Update test for joining net

--- system_test/aest_sync_SUITE.erl
+++ system_test/aest_sync_SUITE.erl
@@ -8,11 +8,13 @@
 -export([end_per_testcase/2]).
 
 % Test cases
--export([new_node_join_old_network/1]).
+-export([new_node_join_old_network/1,
+         crash_and_continue_sync/1]).
 
 -import(aest_nodes, [
     setup_nodes/2,
     start_node/2,
+    stop_node/2,
     request/4,
     wait_for_height/4,
     assert_synchronized/2
@@ -20,7 +22,7 @@
 
 %=== MACROS ====================================================================
 
--define(MINING_TIMOUT, 10000).
+-define(MINING_TIMEOUT, 10000).
 
 -define(NODE1, #{
     name    => node1,
@@ -46,7 +48,9 @@
 %=== COMMON TEST FUNCTIONS =====================================================
 
 all() ->
-    [new_node_join_old_network].
+    [ new_node_join_old_network
+    %% , crash_and_continue_sync 
+    ].
 
 init_per_testcase(_TC, Config) ->
     aest_nodes:ct_setup(Config).
@@ -57,15 +61,37 @@ end_per_testcase(_TC, Config) ->
 %=== TEST CASES ================================================================
 
 new_node_join_old_network(Cfg) ->
+    Length = 10,
     setup_nodes([?NODE1, ?NODE2, ?NODE3], Cfg),
     start_node(node1, Cfg),
     start_node(node2, Cfg),
-    wait_for_height(1, [node1, node2], 2 * ?MINING_TIMOUT, Cfg),
-    io:format(user, "Node 1: ~p~n", [request(node1, [v2, 'block-by-height'], #{height => 1}, Cfg)]),
-    io:format(user, "Node 2: ~p~n", [request(node2, [v2, 'block-by-height'], #{height => 1}, Cfg)]),
-    wait_for_height(5, [node1, node2], 5 * ?MINING_TIMOUT, Cfg),
-    assert_synchronized([node1, node2], Cfg),
+    wait_for_height(Length, [node1, node2], Length * ?MINING_TIMEOUT, Cfg),
+    Top1 = request(node1, [v2, 'top'], #{}, Cfg),
+    ct:log("Node 1 top: ~p~n", [Top1]),
+    Height = request(node2, [v2, 'block-by-height'], #{height => Length}, Cfg),
+    ct:log("Node 2 at height ~p: ~p~n", [Length, Height]),
     start_node(node3, Cfg),
-    wait_for_height(5, [node3], 5 * ?MINING_TIMOUT, Cfg),
-    io:format(user, "Node 3: ~p~n", [request(node3, [v2, 'block-by-height'], #{height => 1}, Cfg)]),
-    assert_synchronized([node1, node3], Cfg).
+    wait_for_height(Length + 1, [node3], ?MINING_TIMEOUT, Cfg),
+    Height = request(node3, [v2, 'block-by-height'], #{height => Length}, Cfg),
+    ok.
+
+crash_and_continue_sync(Cfg) ->
+    %% Create a chain long enough to need some time to fetch
+    Length = 10,
+    setup_nodes([?NODE1, ?NODE2], Cfg),
+    start_node(node1, Cfg),
+    timer:sleep(Length * ?MINING_TIMEOUT),
+    Top1 = request(node1, [v2, 'top'], #{}, Cfg),
+    ct:log("Node 1 top: ~p~n", [Top1]),
+
+    %% Start fetching the chain and crash node1 while doing so
+    start_node(node2, Cfg),
+    wait_for_height(1, [node2], 5000, Cfg),
+    %% we are fetching blocks crash now
+    stop_node(node2, Cfg),
+    Top2 = request(node2, [v2, 'top'], #{}, Cfg),
+    ct:log("Node 2 top: ~p~n", [Top2]),
+    case maps:get(height, Top2) >= maps:get(height,Top1) of
+         true -> {skip, already_synced_when_crashed};
+         false -> ok
+    end.
